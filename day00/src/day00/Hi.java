3a61401a06a484dbeacc779f4c73cc2dcba928cc
ssk1162 <82650238+ssk1162@users.noreply.github.com>
2021-06-22 18:05:13 +0900

    Add files via upload

---
+++ day00/src/day00/Hi.java
@@ -0,0 +1,14 @@
+package day00;
+
+public class Hi {
+
+	public static void main(String[] args) {
+		
+		System.out.println("프");
+		System.out.println("린");
+		System.out.println("트");
+
+		
+	}
+
+}
