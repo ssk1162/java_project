d9ac6638e9253e0ca756eef96d24d3fc7bb47c3c
ssk1162 <82650238+ssk1162@users.noreply.github.com>
2021-06-23 17:58:54 +0900

    Add files via upload

---
+++ day002/src/연산자/비교연산자.java
@@ -0,0 +1,24 @@
+package 연산자;
+
+public class 비교연산자 {
+
+	public static void main(String[] args) {
+		/*
+			비교연산자 : ==, !=, >, >=, <, <=
+			비교결과 중요( 논리데이터, boolean, true/false )
+		*/	
+			
+		int x = 100;
+		int y = 200;
+		
+		System.out.println( x == y);
+		System.out.println( x != y);
+		System.out.println( x >= y);
+		System.out.println( x <= y);
+		System.out.println( x > y);
+		System.out.println( x < y);
+		
+		
+	}
+
+}
