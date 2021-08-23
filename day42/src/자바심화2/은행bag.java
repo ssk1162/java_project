9cc2a0642f934ea0852cd011a4f3bc1e1c7f550c
ssk1162 <82650238+ssk1162@users.noreply.github.com>
2021-08-23 22:49:51 +0900

    Add files via upload

---
+++ day42/src/자바심화2/은행bag.java
@@ -0,0 +1,34 @@
+package 자바심화2;
+
+public class 은행bag {
+	
+	String num;
+	String name;
+	int money;
+	
+	public String getNum() {
+		return num;
+	}
+	public void setNum(String num) {
+		this.num = num;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getMoney() {
+		return money;
+	}
+	public void setMoney(int money) {
+		this.money = money;
+	}
+	@Override
+	public String toString() {
+		return "번호:    " + num + ", 이름:    " + name + ", 은행잔고:    " + money + "\n";
+	}
+	
+	
+	
+}
